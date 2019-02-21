0eef788245915632523d15cbf3e7c11f48985bc1
nahir1409 <47385978+nahir1409@users.noreply.github.com>
2019-02-21 02:28:04 -0300

    Update jsInstruccionSwitch(2).js

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -1,9 +1,30 @@
 function Mostrar()
 {
 //tomo el mes 
-var mesDelAño = document.getElementById('mes').value;
+var mesDelAnio = document.getElementById('mes').value;
 
-alert (mesDelAño);
+
+switch (mesDelAnio) {
+    case "Enero":
+    case "Febrero":
+    case "Marzo":
+    case "Abril":
+    case "Mayo":
+    case "Junio":
+        alert("Falta poco para el invierno");
+        break;
+    case "Julio":
+    case "Agosto":
+        alert("Abrigate que hace frio.");
+        break;
+    case "Septiembre":
+    case "Octubre":
+    case "Noviembre":
+    case "Diciembre":
+        alert("Ya pasamos el ivierno.");
+        break;
+}
+   
 
 
 

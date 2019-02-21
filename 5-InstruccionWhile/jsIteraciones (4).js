429d886b0640cf88d668316410c37fe61e08b692
nahir1409 <47385978+nahir1409@users.noreply.github.com>
2019-02-21 11:42:21 -0300

    Update jsIteraciones (4).js

--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -1,7 +1,17 @@
 function Mostrar()
 {
 
-	var numero = prompt("ingrese un número entre 0 y 10.");
+	
+var numero = parseInt(prompt("ingrese un número entre 0 y 10."));
+	var i = 10;
 
+	//while (numero 0< || numero >10 || isNaN(numero))
+	while (!(numero >=0 || numero <10))
+	{
+	 var numero = parseInt(prompt("Error. Reingrese un numero entre 0 y 10."));
+
+	}
+
+	document.getElementById("Numero").value =numero;
 
 }//FIN DE LA FUNCIÓN

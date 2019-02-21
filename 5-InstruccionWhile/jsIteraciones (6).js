93d0455ff2607e907ee26a3980aff18f76a2c114
nahir1409 <47385978+nahir1409@users.noreply.github.com>
2019-02-21 12:30:02 -0300

    Update jsIteraciones (6).js

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -3,6 +3,14 @@ function Mostrar()
 
 	var contador=0;
 	var acumulador=0;
+	var numero;
+
+while (contador <5) {
+	numero = parseInt(promt("Ingrese un numero: "));
+	acumulador = acumulador + numero;
+	contador++;
+}
+alert(acumulador);
 
 
 

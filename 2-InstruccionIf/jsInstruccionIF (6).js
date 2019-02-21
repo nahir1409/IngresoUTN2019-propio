10b4156c7178bcac6834b6e0f537231fc78050fb
nahir1409 <47385978+nahir1409@users.noreply.github.com>
2019-02-21 02:35:09 -0300

    Update jsInstruccionIF (6).js

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,7 +1,26 @@
 function Mostrar()
 {
 //tomo la edad  
+edad = parseInt(document.getElementById("edad").value);
 
+if (edad <13)
+{
+
+    alert("Es un niño");
+}
+
+else if (edad >= 13 && edad <=17) 
+{
+    alert("Es un adolescente");
+}
+else if (edad >= 18 && edad <=70) 
+{
+    alert("Es un adulto");
+}
+else
+{
+    alert("Es un anciano");
+}
 
 
 

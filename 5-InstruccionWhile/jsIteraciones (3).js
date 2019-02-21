392107d63288c1049be456453aa6e2b9765e0f71
nahir1409 <47385978+nahir1409@users.noreply.github.com>
2019-02-21 10:47:06 -0300

    Update jsIteraciones (3).js

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -2,6 +2,14 @@ function Mostrar()
 {
 
 var clave = prompt("ingrese el número clave.");
+ 
+ //while (clave != "utn750")
+ while ( !(clave == "utn750"))
+ {
+     clave = prompt("Conraseña Inválida. Intente de nuevo: ");
+ }
+
+ alert("¡Contraseña Correcta!");
 
 
 }//FIN DE LA FUNCIÓN

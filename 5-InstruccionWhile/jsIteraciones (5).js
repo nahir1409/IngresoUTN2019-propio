34093de465ccfc23ccfd0332cf10fc37fde0dbc3
nahir1409 <47385978+nahir1409@users.noreply.github.com>
2019-02-21 12:06:32 -0300

    Update jsIteraciones (5).js

--- 5-InstruccionWhile/jsIteraciones (5).js
+++ 5-InstruccionWhile/jsIteraciones (5).js
@@ -1,10 +1,24 @@
 function Mostrar()
 {
 
-var sexo = prompt("ingrese f ó m .");
+//var sexo = prompt("ingrese f ó m .").toLowerCase(); 
+//while (!(sexo == "f" || sexo == "m"))
+//	{
+//	
+//  sexo = prompt("Error. Reingrese ingrese f ó m.").toLowerCase();
+//
+//	}
+  
+ var sexo = prompt("ingrese f ó m .");
 
 
+//while ( sexo != "f" && sexo != "m") otra forma correcta de hacerlo
+while (!(sexo == "f" || sexo == "m"))
+	{	
+    sexo = prompt("Error. Reingrese ingrese f ó m.");
+	}
 
 document.getElementById('Sexo').value=sexo;
+
 
 }//FIN DE LA FUNCIÓN

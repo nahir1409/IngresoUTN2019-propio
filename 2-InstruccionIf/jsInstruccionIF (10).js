96efa717d46a54937c6db1e2294698597747eca7
nahir1409 <47385978+nahir1409@users.noreply.github.com>
2019-02-21 02:32:32 -0300

    Update jsInstruccionIF (10).js

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,19 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+		var nota;
+	nota = Math.floor(Math.random() * 10 + 1);
+
+	if (nota >=9) {
+		alert("Nota: " + nota + " Exelente");
+	}
+	else {
+		if (nota >3) {
+		alert("Nota: " + nota + " Aprobó");
+		}
+		else {
+			alert("nota: " + nota + " Vamos, la proxima se puede");
+		}
+
 
 }//FIN DE LA FUNCIÓN

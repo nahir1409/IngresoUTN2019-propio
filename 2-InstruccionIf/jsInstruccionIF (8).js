e16016b785a027235bc96e7c02de5d4fb72ae91f
nahir1409 <47385978+nahir1409@users.noreply.github.com>
2019-02-21 02:33:43 -0300

    Update jsInstruccionIF (8).js

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,7 +1,17 @@
 function Mostrar()
 {
 //tomo la edad  
+var edad;
+var estado;
 
+edad = parseInt(document.getElementById("edad").value);
+estado = document.getElementById("estadoCivil").value;
+
+if ( estado == "Soltero" && edad >=18 )
+{
+    alert("Es soltero y no es menor.");
+}
+	
 	
 
 

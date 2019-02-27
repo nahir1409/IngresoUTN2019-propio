d1ec923a7184d3ad67779136fdfe631020c192a9
nahir1409 <47385978+nahir1409@users.noreply.github.com>
2019-02-27 11:57:15 -0300

    Update jsIteraciones (9).js

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,16 +1,45 @@
 function Mostrar()
 {
-
-	var contador=0;
 	// declarar variables
-	
+
+	var numero;
+	var maximo;
+	var minimo;
+	var contador=0;
 	var respuesta='si';
 
-	while(respuesta!='no')
+/*	while(respuesta!='no')
+	{			
+	}while (respuesta == "si");*/
+
+
+    do
 	{
+		numero = parseInt(prompt("Ingrese un número: "));
+
+		while (isNaN(numero)) 
+		{
+			numero = parseInt(prompt("Esto no es un número. Ingrese un número: "));
+		} 
+
+		if (maximo >=numero) 
+		{
+
+		}
+		else 
+		{
+          minimo
+		}
+
+		respuesta = prompt("¿Desea continuar?");
 		
+	}while (respuesta == "si");
+
+
+	document.getElementById("maximo").value = maximo;
+	document.getElementById("minimo").value = minimo;
 	
-	}
+	
 
 
 

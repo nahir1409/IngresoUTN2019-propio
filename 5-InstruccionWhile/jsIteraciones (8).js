32de41a752f866a9847451d21078e93cef4e97a6
nahir1409 <47385978+nahir1409@users.noreply.github.com>
2019-02-27 10:17:49 -0300

    Update jsIteraciones (8).js

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -1,14 +1,36 @@
 function Mostrar()
 {
 
-	var contador=0;
 	var positivo=0;
 	var negativo=1;
-	
 	var respuesta='si';
+	var numero;
+	var promedio;
+	var contadorNeg;
 
+	 do {
+		numero = parseInt(prompt("Ingrese un número: "));
 
+		while (isNaN(numero)) {
+			numero = parseInt(prompt("Esto no es un número. Ingrese un número: "));
+		}
+            if ( numero >= 0) {
+				positivo = positivo + numero;
+			}
+			else  {
+				negativo = negativo * numero;
+				contadorNeg++;
+			}
+
+			respuesta = prompt("¿Desea continuar?");
+		
+	    }while (respuesta == "si");
+	
 document.getElementById('suma').value=positivo;
+   if (contadorNeg == 0 ) {
+	   negativo = 0;
+   }
 document.getElementById('producto').value=negativo;
+
 
 }//FIN DE LA FUNCIÓN

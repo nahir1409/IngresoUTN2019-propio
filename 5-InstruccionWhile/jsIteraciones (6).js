d665232737c8e6a306d7da6d3a8b0748a8753a47
nahir1409 <47385978+nahir1409@users.noreply.github.com>
2019-02-27 10:16:23 -0300

    Update jsIteraciones (6).js

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -1,7 +1,7 @@
 function Mostrar()
 {
 
-	var contador=0;
+	/*var contador=0;
 	var acumulador=0;
 	var numero;
 
@@ -15,6 +15,26 @@ alert(acumulador);
 
 
 document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/5;
+document.getElementById('promedio').value=acumulador/5;*/
+	
+	
+	
+	
+	var contador=0;
+	var acumulador=0;
+	var numero;
+	var promedio;
+
+while (contador <5) {
+	numero = parseInt(prompt("Ingrese un numero: "));
+	acumulador = acumulador + numero;
+	contador++;
+}
+
+promedio = acumulador / 5;
+
+
+document.getElementById('suma').value=acumulador;
+document.getElementById('promedio').value=promedio;
 
 }//FIN DE LA FUNCIÓN

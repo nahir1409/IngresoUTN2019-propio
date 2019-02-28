0f2fc5e357ed57f51c0d2da5787357dc880e28a0
nahir1409 <47385978+nahir1409@users.noreply.github.com>
2019-02-28 09:23:30 -0300

    Update jsIteracionesFor (6).js

--- 6-InstruccionFor/jsIteracionesFor (6).js
+++ 6-InstruccionFor/jsIteracionesFor (6).js
@@ -1,6 +1,27 @@
 function Mostrar()
 {
+var numero;
+    var contador=0;
+    
+    numero = parseInt(prompt("ingrese un número positivo."));
+    
+      while (numero <=0 || isNaN(numero)) {
 
+          numero = parseImt(prompt("Error. ingrese un número positivo."));
+      }
+
+    for ( var i=1; i<= numero ; i++ ) 
+    {
+          if (i%2==0){
+              //contar y mostrar
+              contador++;
+              console.log(i);
+          }
+    
+      
+    }
+
+console.log("Cantidad de pares: " + contador);
 
 
 

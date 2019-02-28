68fcda8009d3bf4ce77b2b75af47493a3bac8c61
nahir1409 <47385978+nahir1409@users.noreply.github.com>
2019-02-28 08:51:28 -0300

    Update jsIteracionesFor (4).js

--- 6-InstruccionFor/jsIteracionesFor (4).js
+++ 6-InstruccionFor/jsIteracionesFor (4).js
@@ -1,6 +1,24 @@
 function Mostrar()
 {
 
+for (var i = 0; i< 100; i++)
+{
+    if( i ==10) {
+        break;
+
+    } console.log(i);
+}
+
+
+
+/*for (var i = 0; i< 100; i++)
+{
+ console.log(i);
+}
+    if( i ==10) {
+        break;
+
+}*/
 
 
 

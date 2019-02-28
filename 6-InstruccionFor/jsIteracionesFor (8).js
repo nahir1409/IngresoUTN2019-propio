726c0f2ac6016daed18c4ba3634a74d70c2d659d
nahir1409 <47385978+nahir1409@users.noreply.github.com>
2019-02-28 09:48:58 -0300

    Update jsIteracionesFor (8).js

--- 6-InstruccionFor/jsIteracionesFor (8).js
+++ 6-InstruccionFor/jsIteracionesFor (8).js
@@ -1,7 +1,15 @@
 function Mostrar()
 {
 
+  var numero;
+    var contador=0;
+    
+    numero = parseInt(prompt("ingrese un número positivo."));
+    
+      while (numero >=2 || isNaN(numero)) {
+          alert( "No es un número PRIMO.")
 
+          numero = parseImt(prompt("Reingrese un número positivo."));
 
 
 }//FIN DE LA FUNCIÓN
